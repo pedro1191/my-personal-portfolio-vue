d9fc42c89cbd93dccdaf18c9e18dbf0b3e232192
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-03 10:34:14 -0300

    Add divider and title components

---
+++ src/components/Divider.vue
@@ -0,0 +1,42 @@
+<template>
+  <hr class="icon star-of-life" :style="customStyle">
+</template>
+
+<script>
+export default {
+  props: {
+    customStyle: Object
+  }
+};
+</script>
+
+<style scoped>
+hr {
+  max-width: 15rem;
+  border: none;
+  border-top: solid 0.25rem;
+  background-color: inherit;
+  padding: 0;
+  margin: 2rem auto;
+  color: inherit;
+}
+
+.icon::before {
+  display: inline-block;
+  font-style: normal;
+  font-variant: normal;
+  text-rendering: auto;
+  -webkit-font-smoothing: antialiased;
+  background-color: inherit;
+}
+
+.star-of-life::before {
+  font-family: 'Font Awesome 5 Free';
+  font-weight: 900;
+  content: '\f621';
+  position: relative;
+  top: -1.6rem;
+  font-size: 2rem;
+  padding: 0 0.25em;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :style="getWrapperStyle">
+    <h1 :style="getTitleStyle">{{ title }}</h1>
+    <app-divider :customStyle="getDividerStyle" />
+  </div>
+</template>
+
+<script>
+import Divider from './Divider.vue';
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    customStyle: Object
+  },
+  computed: {
+    getWrapperStyle() {
+      return this.customStyle && this.customStyle.wrapper
+        ? this.customStyle.wrapper
+        : {};
+    },
+    getTitleStyle() {
+      return this.customStyle && this.customStyle.title
+        ? this.customStyle.title
+        : {};
+    },
+    getDividerStyle() {
+      return this.customStyle && this.customStyle.divider
+        ? this.customStyle.divider
+        : {};
+    }
+  },
+  components: {
+    appDivider: Divider
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+}
+</style>
+
+
