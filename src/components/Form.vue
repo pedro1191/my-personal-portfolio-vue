a76a0f8602b798925dc9158d85d65f1ff3509e95
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-03 16:35:01 -0300

    Add form components

---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="form" novalidate="novalidate">
+    <app-form-group v-for="(formGroup, index) in formGroups" :key="index" :formGroup="formGroup" @formSubmited="$emit('formSubmited')" />
+  </form>
+</template>
+
+<script>
+import FormGroup from './FormGroup.vue';
+
+export default {
+  props: {
+    formGroups: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    appFormGroup: FormGroup
+  }
+};
+</script>
+
+<style scoped>
+.form {
+  text-align: left;
+}
+</style>
+
---
+++ src/components/FormGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="form-group mb-0 py-2">
+    <label class="label" :class="{ 'show-label': formGroup.value, 'text-danger': formGroup.validationMessage }" v-if="formGroup.label">{{ formGroup.label }}</label>
+
+    <textarea class="form-control form-control-lg" rows="5" v-model="formGroup.value" :disabled="formGroup.disabled" :placeholder="formGroup.description" :class="{ 'is-invalid': formGroup.validationMessage }" v-if="formGroup.type === 'textarea'"></textarea>
+    <button class="btn btn-info btn-lg" type="submit" :disabled="formGroup.disabled" @click.prevent="$emit('formSubmited')" v-else-if="formGroup.type === 'submit'">{{ formGroup.description }}</button>
+    <input class="form-control form-control-lg" type="text" v-model="formGroup.value" :placeholder="formGroup.description" :disabled="formGroup.disabled" :class="{ 'is-invalid': formGroup.validationMessage }" v-else>
+
+    <div class="invalid-feedback" v-if="formGroup.validationMessage">{{ formGroup.validationMessage }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    formGroup: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.label {
+  margin: 0;
+  padding: 0 0 0 1rem;
+  color: #17a2b8;
+  opacity: 0;
+}
+
+.show-label {
+  opacity: 1;
+}
+</style>
+
+
