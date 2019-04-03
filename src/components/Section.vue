aedaf02edb9d46bc86ba1fb7174eefcb7ad6adc1
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-03 11:01:38 -0300

    Add section component

---
+++ src/components/Section.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container section" :style="getWrapperStyle">
+    <app-title :title="title" :customStyle="getTitleStyle" v-if="title" />
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+import Title from './Title.vue';
+
+export default {
+  props: {
+    title: String,
+    customStyle: Object
+  },
+  components: {
+    appTitle: Title
+  },
+  computed: {
+    getWrapperStyle() {
+      return this.customStyle && this.customStyle.wrapper
+        ? this.customStyle.wrapper
+        : {};
+    },
+    getTitleStyle() {
+      return this.customStyle && this.customStyle.title
+        ? this.customStyle.title
+        : {};
+    }
+  }
+};
+</script>
+
+<style scoped>
+.section {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+</style>
+
