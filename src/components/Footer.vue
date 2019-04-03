a3bca4147d13bdd8aa0b879bc71a3df032d9857a
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-03 10:32:24 -0300

    Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="mt-auto bg-dark-secondary">
+    <div class="container d-flex py-3">
+      <p class="m-0 text-muted">Pedro de Almeida's Portfolio {{ thisYear }}</p>
+      <app-social-media class="ml-auto" :socialMedia="socialMedia" />
+    </div>
+  </footer>
+</template>
+
+<script>
+import Header from './Header.vue';
+import SocialMedia from './SocialMedia.vue';
+
+export default {
+  data() {
+    return {
+      socialMedia: [
+        {
+          name: 'LinkedIn',
+          link: 'https://www.linkedin.com/in/phdealmeida/',
+          iconClass: 'fab fa-linkedin-in'
+        },
+        {
+          name: 'GitHub',
+          link: 'https://github.com/pedro1191',
+          iconClass: 'fab fa-github'
+        }
+      ]
+    };
+  },
+  computed: {
+    thisYear() {
+      return new Date().getFullYear();
+    }
+  },
+  components: {
+    appHeader: Header,
+    appSocialMedia: SocialMedia
+  }
+};
+</script>
+
+<style scoped>
+.bg-dark-secondary {
+  background-color: #1a252f;
+}
+</style>
+
---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="social-media text-muted">
+    <a v-for="(sm, index) in socialMedia" :key="index" :href="sm.link" target="_blank" rel="noreferrer noopener">
+      <i :class="sm.iconClass" :title="sm.name"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    socialMedia: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.social-media {
+  display: flex;
+  align-content: center;
+  align-items: center;
+}
+
+.social-media a {
+  text-decoration: none;
+  color: inherit;
+  margin: 0 0.5rem;
+}
+
+.social-media a:hover,
+.social-media a:active {
+  color: #fff;
+}
+</style>
