19aad759cdd9adef7c51464bc19a3cf0889dece8
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-03 10:30:58 -0300

    Update global layout

--- src/views/GlobalLayout.vue
+++ src/views/GlobalLayout.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    <app-header />
-    <router-view />
+  <div class="d-flex flex-column h-100">
+    <app-navbar :navLinks="navLinks" />
+    <router-view class="router-view" />
+    <app-footer />
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue';
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
-  name: 'globalLayout',
+  data() {
+    return {
+      navLinks: [
+        { name: 'PORTFOLIO', link: { name: 'home', hash: '#portfolio' } },
+        { name: 'ABOUT', link: { name: 'home', hash: '#about' } },
+        { name: 'CONTACT', link: { name: 'home', hash: '#contact' } }
+      ]
+    };
+  },
   components: {
-    appHeader: Header
+    appNavbar: Navbar,
+    appFooter: Footer
   }
 };
 </script>
+
+<style scoped>
+.router-view {
+  padding-top: 74px;
+}
+</style>
