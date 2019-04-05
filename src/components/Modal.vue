a07ba4c0d9efa7cc2e5a26ad9a2edadddee989d4
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-05 20:47:43 -0300

    Add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,87 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-content">
+
+            <div class="modal-header">
+              <slot name="header"></slot>
+            </div>
+
+            <div class="modal-body">
+              <slot name="body"></slot>
+            </div>
+
+            <div class="modal-footer">
+              <slot name="footer"></slot>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+
+.modal-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.modal-content {
+  padding: 0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  margin: 2.5%;
+  border-radius: 0;
+  width: auto;
+}
+
+.modal-header:empty,
+.modal-footer:empty {
+  display: none;
+}
+
+/*
+ * Modal transition
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
