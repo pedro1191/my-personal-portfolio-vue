2b6a2c9e5eb40f51b8cf4b0990c225f7fec62832
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-07 12:48:32 -0300

    Add spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="loader">
+    <i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i>
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
+
+<style scoped>
+.loader {
+  color: #fff;
+}
+</style>
