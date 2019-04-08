bf784f393af505e9ec0f5cd263ec33031d738042
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-08 12:24:49 -0300

    Update navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,16 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark" :class="{ 'navbar-shrink': !scrollOnTop }">
     <div class="container">
-      <a class="navbar-brand" href="#">
-        <app-logo />
+      <a class="navbar-brand" title="Home" href="#home">
+        <app-logo class="app-logo" />
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-label="Toggle navigation" :aria-expanded="openMenu" :class="{ collapsed: !openMenu }" @click="onNavbarTogglerClick">
+        {{ togglerTitle }}<i class="fas fa-bars ml-2"></i>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <div class="collapse navbar-collapse" id="navbarNavDropdown" :class="{ show: openMenu }">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Portfolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+          <li v-for="(navLink, index) in navLinks" :key="index" class="nav-item">
+            <a :href="navLink.link.hash" class="nav-link">{{ navLink.name }}</a>
           </li>
         </ul>
       </div>
@@ -28,9 +22,172 @@
 import Logo from './Logo.vue';
 
 export default {
-  name: 'navbar',
+  props: {
+    togglerTitle: {
+      type: String,
+      default: 'Menu'
+    },
+    navLinks: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      scrollYPosition: null,
+      openMenu: false,
+      hashLinks: []
+    };
+  },
+  computed: {
+    scrollOnTop: function() {
+      return this.scrollYPosition <= 100;
+    },
+    viewportOffset: function() {
+      return 200;
+    }
+  },
   components: {
     appLogo: Logo
+  },
+  methods: {
+    updateScroll() {
+      this.scrollYPosition = window.scrollY;
+    },
+    updateNavLinkClass() {
+      for (let i = 0; i < this.hashLinks.length; i++) {
+        const element = document.querySelector(
+          `[ href="#${this.hashLinks[i]}" ]`
+        );
+
+        if (this.isInViewport(this.hashLinks[i])) {
+          element.parentElement.classList.add('active');
+        } else {
+          element.parentElement.classList.remove('active');
+        }
+      }
+    },
+    onNavbarTogglerClick() {
+      this.openMenu = !this.openMenu;
+    },
+    isInViewport(id) {
+      const element = document.getElementById(id);
+      const bounding = element.getBoundingClientRect();
+
+      return (
+        bounding.top <= this.viewportOffset &&
+        bounding.bottom >= this.viewportOffset &&
+        bounding.left >= 0 &&
+        bounding.right <=
+          (window.innerWidth || document.documentElement.clientWidth)
+      );
+    },
+    scroll(from, to, hash, html, body) {
+      const timeInterval = 1; // in ms
+      let prevScrollTop;
+      const increment = to > from ? 20 : -20;
+      const vm = this;
+
+      const scrollByPixel = setInterval(function() {
+        const scrollTop = Math.round(body.scrollTop || html.scrollTop); // getting current scroll position
+
+        if (
+          prevScrollTop == scrollTop ||
+          (to > from && scrollTop >= to) ||
+          (to < from && scrollTop <= to)
+        ) {
+          clearInterval(scrollByPixel);
+
+          window.location.hash = hash; // Add hash to the url after scrolling
+        } else {
+          body.scrollTop += increment;
+          html.scrollTop += increment;
+
+          prevScrollTop = scrollTop;
+        }
+      }, timeInterval);
+    },
+    onAnchorClick(event, html, body, hash, hashElement) {
+      const scrollTop = Math.round(body.scrollTop || html.scrollTop); // getting current scroll position
+
+      event.preventDefault(); // preventing default anchor click behavior
+
+      let hashElementTop = 0;
+      let obj = hashElement;
+
+      while (obj.offsetParent) {
+        hashElementTop += obj.offsetTop;
+        obj = obj.offsetParent;
+      }
+
+      hashElementTop = Math.round(hashElementTop); // getting element's position
+
+      this.scroll(scrollTop, hashElementTop, hash, html, body);
+    },
+    addSmoothScrollOnAnchorClick() {
+      const links = document.links;
+      const html = document.documentElement;
+      const body = document.body;
+      const vm = this;
+
+      for (let i = 0; i < links.length; i++) {
+        const href = links[i].getAttribute('href');
+        const id = href.substring(1);
+        const hashElement = document.getElementById(id);
+
+        if (hashElement) {
+          this.hashLinks.push(id);
+          links[i].onclick = event =>
+            this.onAnchorClick(event, html, body, href, hashElement);
+        }
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', () => {
+      this.updateScroll();
+      this.updateNavLinkClass();
+    });
+
+    this.addSmoothScrollOnAnchorClick();
+  },
+  watch: {
+    $route() {
+      this.openMenu = false;
+    }
   }
 };
 </script>
+
+<style scoped>
+.fa-bars {
+  font-size: 1.5rem;
+}
+
+.navbar.navbar-shrink {
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+}
+
+.app-logo,
+.navbar-shrink .app-logo {
+  height: 3rem;
+  transition: height 0.25s;
+}
+
+@media (min-width: 768px) {
+  .navbar {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  .app-logo {
+    height: 4rem;
+  }
+
+  .navbar-nav .active > .nav-link {
+    color: #fff;
+    background-color: #17a2b8;
+  }
+}
+</style>
