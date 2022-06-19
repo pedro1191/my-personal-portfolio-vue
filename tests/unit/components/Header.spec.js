ca96e065a5f42b8cc10524a02c9e3859799489f2
Pedro Almeida <pedro_almeida1191@hotmail.com>
2022-06-19 19:28:10 -0300

    test: add unit tests for the Header component

---
+++ tests/unit/components/Header.spec.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker';
+import { mount } from '@vue/test-utils';
+import Header from '@/components/Header.vue';
+
+describe('Header.vue', () => {
+  it('renders props.customStyle when passed', () => {
+    // ARRANGE
+    const colorFormat = { format: 'css' };
+    const customStyle = {
+      color: faker.color.rgb(colorFormat),
+    };
+    const wrapper = mount(Header, {
+      props: { customStyle },
+    });
+
+    // ACT
+    const wrapperStyle = wrapper.attributes('style');
+
+    // ASSERT
+    expect(wrapperStyle).toBe(`color: ${customStyle.color};`);
+  });
+
+  it('renders the default slot content', () => {
+    // ARRANGE
+    const content = faker.lorem.words();
+    const wrapper = mount(Header, {
+      slots: {
+        default: content,
+      },
+    });
+
+    // ACT
+    const wrapperText = wrapper.text();
+
+    // ASSERT
+    expect(wrapperText).toBe(content);
+  });
+});
