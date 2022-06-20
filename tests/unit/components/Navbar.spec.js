5a63a7eab5876b634f6d11bfb044842023c01d04
Pedro Almeida <pedro_almeida1191@hotmail.com>
2022-06-20 08:35:26 -0300

    test: add unit tests for the Navbar component

---
+++ tests/unit/components/Navbar.spec.js
@@ -0,0 +1,62 @@
+import { faker } from '@faker-js/faker';
+import { mount } from '@vue/test-utils';
+import { RouterLinkStub } from '@vue/test-utils';
+import Navbar from '@/components/Navbar.vue';
+
+describe('Navbar.vue', () => {
+  it('renders props.togglerTitle when passed', () => {
+    // ARRANGE
+    const togglerTitle = faker.lorem.words();
+    const navLinks = [];
+    const wrapper = mount(Navbar, {
+      global: {
+        stubs: ['FontAwesomeIcon', 'RouterLink'],
+      },
+      props: { togglerTitle, navLinks },
+    });
+
+    // ACT
+    const wrapperText = wrapper.text();
+
+    // ASSERT
+    expect(wrapperText).toMatch(togglerTitle);
+  });
+
+  it('renders props.navLinks when passed', () => {
+    // ARRANGE
+    const numberOfItems = faker.datatype.number({ min: 1, max: 3 });
+    const navLinks = [];
+    for (let i = 0; i < numberOfItems; i++) {
+      navLinks.push({
+        name: faker.lorem.word(),
+        reference_id: faker.lorem.word(),
+        link: {
+          hash: `#${faker.lorem.word()}`,
+        },
+      });
+    }
+    const wrapper = mount(Navbar, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+          RouterLink: RouterLinkStub,
+        },
+      },
+      props: { navLinks },
+    });
+
+    // ACT
+    const anchors = wrapper.findAllComponents(RouterLinkStub);
+    anchors.splice(0, 1); // ignores the default home link (always the first one)
+
+    // ASSERT
+    expect(navLinks.length).toBe(anchors.length);
+    anchors.forEach((anchor, index) => {
+      expect(anchor.text()).toBe(navLinks[index].name);
+      expect(anchor.props().to).toEqual({
+        name: 'home',
+        hash: navLinks[index].link.hash,
+      });
+    });
+  });
+});
