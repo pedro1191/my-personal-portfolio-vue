5bff905eb0db4e3963cac698ab064cbb0fa960c7
Pedro Almeida <pedro_almeida1191@hotmail.com>
2022-07-03 08:28:59 -0300

    feat: add FeedbackMessageModal component

---
+++ src/components/FeedbackMessageModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <Backdrop @maskClick="emitClose">
+    <ZoomAnimation>
+      <Modal
+        v-if="show"
+        :customContentStyle="customContentStyle"
+        @close="emitClose"
+      >
+        <template v-slot:body>
+          <slot></slot>
+        </template>
+        <template v-slot:footer>
+          <button title="Close" class="btn btn-info" @click="emitClose">
+            Ok
+          </button>
+        </template>
+      </Modal>
+    </ZoomAnimation>
+  </Backdrop>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modal';
+import Backdrop from '@/components/Backdrop.vue';
+import Modal from '@/components/Modal.vue';
+import ZoomAnimation from '@/components/ZoomAnimation.vue';
+
+export default {
+  name: 'AppFeedbackMessage',
+  components: {
+    Backdrop,
+    Modal,
+    ZoomAnimation,
+  },
+  mixins: [modalMixin],
+  props: {
+    customContentStyle: Object,
+  },
+};
+</script>
---
+++ tests/unit/components/FeedbackMessageModal.spec.js
@@ -0,0 +1,40 @@
+import { faker } from '@faker-js/faker';
+import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import { TIMEOUT_MS } from '@/mixins/modal';
+import FeedbackMessageModal from '@/components/FeedbackMessageModal.vue';
+
+describe('FeedbackMessageModal.vue', () => {
+  it('renders a FeedbackMessageModal', async () => {
+    // ARRANGE
+    const defaultContent = faker.lorem.words();
+    const wrapper = mount(FeedbackMessageModal, {
+      slots: {
+        default: defaultContent,
+      },
+    });
+
+    // ACT
+    await nextTick();
+    const wrapperText = wrapper.text();
+
+    // ASSERT
+    expect(wrapperText).toContain(defaultContent);
+  });
+
+  it('emits the close event when the button is clicked', (done) => {
+    // ARRANGE
+    const wrapper = mount(FeedbackMessageModal);
+
+    // ACT
+    wrapper.vm.$nextTick(async () => {
+      await wrapper.get('button').trigger('click');
+
+      // ASSERT
+      setTimeout(() => {
+        expect(wrapper.emitted().close).toBeTruthy();
+        done();
+      }, TIMEOUT_MS);
+    });
+  });
+});
