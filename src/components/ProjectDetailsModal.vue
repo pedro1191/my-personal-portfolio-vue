44706c73e9ed6453e0580cfa7d7b44a7e7ee964d
Pedro Almeida <pedro_almeida1191@hotmail.com>
2022-07-03 08:36:50 -0300

    feat: add ProjectDetailsModal component

---
+++ src/components/ProjectDetailsModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <Backdrop @maskClick="emitClose">
+    <ZoomAnimation>
+      <Modal
+        v-if="show"
+        :customContentStyle="customContentStyle"
+        @close="emitClose"
+      >
+        <template v-slot:header>
+          <div class="close-button">
+            <button title="Close" @click="emitClose">
+              <font-awesome-icon class="ml-2" icon="fa-times" size="3x" />
+            </button>
+          </div>
+        </template>
+        <template v-slot:body>
+          <slot></slot>
+        </template>
+      </Modal>
+    </ZoomAnimation>
+  </Backdrop>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modal';
+import Backdrop from '@/components/Backdrop.vue';
+import Modal from '@/components/Modal.vue';
+import ZoomAnimation from '@/components/ZoomAnimation.vue';
+
+export default {
+  name: 'AppProjectDetails',
+  components: {
+    Backdrop,
+    Modal,
+    ZoomAnimation,
+  },
+  mixins: [modalMixin],
+  props: {
+    customContentStyle: Object,
+  },
+};
+</script>
+
+<style scoped>
+.close-button {
+  padding: 0;
+  width: 100%;
+  text-align: right;
+}
+
+.close-button button {
+  border: none;
+  outline: none;
+  background-color: transparent;
+  color: #17a2b8;
+  display: inline-flex;
+  align-items: center;
+}
+
+.close-button button:hover,
+.close-button button:focus {
+  color: #117a8b;
+}
+</style>
---
+++ tests/unit/components/ProjectDetailsModal.spec.js
@@ -0,0 +1,47 @@
+import { faker } from '@faker-js/faker';
+import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+import { TIMEOUT_MS } from '@/mixins/modal';
+import ProjectDetailsModal from '@/components/ProjectDetailsModal.vue';
+
+describe('ProjectDetailsModal.vue', () => {
+  it('renders a ProjectDetailsModal', async () => {
+    // ARRANGE
+    const defaultContent = faker.lorem.words();
+    const wrapper = mount(ProjectDetailsModal, {
+      global: {
+        stubs: ['FontAwesomeIcon'],
+      },
+      slots: {
+        default: defaultContent,
+      },
+    });
+
+    // ACT
+    await nextTick();
+    const wrapperText = wrapper.text();
+
+    // ASSERT
+    expect(wrapperText).toContain(defaultContent);
+  });
+
+  it('emits the close event when the button is clicked', (done) => {
+    // ARRANGE
+    const wrapper = mount(ProjectDetailsModal, {
+      global: {
+        stubs: ['FontAwesomeIcon'],
+      },
+    });
+
+    // ACT
+    wrapper.vm.$nextTick(async () => {
+      await wrapper.get('button').trigger('click');
+
+      // ASSERT
+      setTimeout(() => {
+        expect(wrapper.emitted().close).toBeTruthy();
+        done();
+      }, TIMEOUT_MS);
+    });
+  });
+});
