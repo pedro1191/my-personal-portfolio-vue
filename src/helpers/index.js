6c16c2c9680d399852935b8f44514396225730b2
Pedro de Almeida <pedro_almeida1191@hotmail.com>
2025-06-07 15:31:55 +0100

    feat: add theme toggle

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,5 @@
+import { Theme } from '@/constants';
+
 export const getRandomBackgroundColor = (min = 100000, max = 999999) => {
   const minValue = Math.ceil(min);
   const maxValue = Math.floor(max);
@@ -5,4 +7,8 @@ export const getRandomBackgroundColor = (min = 100000, max = 999999) => {
     Math.floor(Math.random() * (maxValue - minValue)) + minValue
   }`;
   return hexColor;
+};
+
+export const isValidTheme = (theme) => {
+  return Object.values(Theme).includes(theme);
 };
