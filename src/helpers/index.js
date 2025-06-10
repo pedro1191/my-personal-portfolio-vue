66928a8bdfef686647f20a70db399ebe1dda1923
Pedro de Almeida <pedro_almeida1191@hotmail.com>
2025-06-10 11:13:35 +0100

    chore: remove unused

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,14 +1,5 @@
 import { Theme } from '@/constants';
 
-export const getRandomBackgroundColor = (min = 100000, max = 999999) => {
-  const minValue = Math.ceil(min);
-  const maxValue = Math.floor(max);
-  const hexColor = `#${
-    Math.floor(Math.random() * (maxValue - minValue)) + minValue
-  }`;
-  return hexColor;
-};
-
 export const isValidTheme = (theme) => {
   return Object.values(Theme).includes(theme);
 };
--- tests/unit/helpers/index.spec.js
+++ tests/unit/helpers/index.spec.js
@@ -1,24 +1,9 @@
 import { faker } from '@faker-js/faker';
 import { describe, expect, it } from 'vitest';
 import { Theme } from '@/constants';
-import { getRandomBackgroundColor, isValidTheme } from '@/helpers';
+import { isValidTheme } from '@/helpers';
 
 describe('getRandomBackgroundColor', () => {
-  it('generates a random hex color', () => {
-    // ARRANGE
-    const minValue = 100000;
-    const maxValue = 999999;
-
-    // ACT
-    const hexColor = getRandomBackgroundColor(minValue, maxValue);
-    const hexColorNumber = parseInt(hexColor.replace(/[^\d]/, ''));
-
-    // ASSERT
-    expect(hexColor).toBe(`#${hexColorNumber}`);
-    expect(hexColorNumber).toBeGreaterThanOrEqual(minValue);
-    expect(hexColorNumber).toBeLessThanOrEqual(maxValue);
-  });
-
   it('validates the themes', () => {
     // ARRANGE
     const darkTheme = Theme.Dark;
